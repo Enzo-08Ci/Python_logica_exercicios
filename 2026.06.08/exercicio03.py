65a7565d6ff3a2c807636926e1cc338cf526b614
André Luferat <contato@luferat.net>
2026-06-10 13:58:22 -0300

    Update exercicio03.py

--- 2026.06.08/exercicio03.py
+++ 2026.06.08/exercicio03.py
@@ -10,9 +10,26 @@ Depois:
  - Adicione algumas frutas e exiba a última, independente da quantidade.
 '''
 
+# Crie uma lista contendo pelo menos 5 frutas.
 fruits = ["banana", "laranja", "kiwi",
           "abacate", "tangerina", "morango", "pêra"]
+
+# Exiba a lista completa.
+print(fruits)
+
+# Exiba apenas a primeira fruta.
 print(fruits[0])
-print(fruits[4])
-# Obtém o último elemento da lista independente da quantidade
+
+# Exiba apenas a última fruta.
+print(fruits[4])  
+
+# Adicione algumas frutas 
+fruits.append("uva")
+fruits.append("melancia")
+fruits.append("abacaxi")
+
+# e exiba a última, independente da quantidade.
 print(fruits[len(fruits) - 1])
+# outra forma de obter o último elemento da lista é usando índice negativo
+print(fruits[-1])  
+
